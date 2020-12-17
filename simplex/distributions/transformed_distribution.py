401340766462dbdaf45c680d57c206ee8be75b38
Feynman Liang <feynmanliang@users.noreply.github.com>
2020-12-17 12:05:49 -0800

    Fix sign error in TransformedDistribution.log_prob
    Fixes #12

--- simplex/distributions/transformed_distribution.py
+++ simplex/distributions/transformed_distribution.py
@@ -54,7 +54,7 @@ class TransformedDistribution(torch.distributions.Distribution):
         event_dim = len(self.event_shape)
         
         x = self.bijector.inverse(y, self.params)
-        log_prob = _sum_rightmost(self.bijector.log_abs_det_jacobian(x, y, self.params), event_dim - self.bijector.event_dim)
+        log_prob = -_sum_rightmost(self.bijector.log_abs_det_jacobian(x, y, self.params), event_dim - self.bijector.event_dim)
         log_prob = log_prob + _sum_rightmost(self.base_dist.log_prob(x), event_dim - len(self.base_dist.event_shape))
 
         #self.cache(y,)
