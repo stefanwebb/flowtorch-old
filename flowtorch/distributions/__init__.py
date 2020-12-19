73c7215be46b8ad5232e9692368b8d6b8570f933
Stefan Webb <info@stefanwebb.me>
2020-12-18 16:42:16 -0800

    isort

--- flowtorch/distributions/__init__.py
+++ flowtorch/distributions/__init__.py
@@ -1,9 +1,7 @@
 # Copyright (c) FlowTorch Development Team. All Rights Reserved
 # SPDX-License-Identifier: MIT
 
-from flowtorch.distributions.transformed_distribution import (
-    TransformedDistribution,
-)
+from flowtorch.distributions.transformed_distribution import TransformedDistribution
 
 __all__ = [
     "TransformedDistribution",
