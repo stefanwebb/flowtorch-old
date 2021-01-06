0e76828d9b513152c513b16fc9ee568d23ee0876
Feynman Liang <feynman.liang@gmail.com>
2021-01-06 00:08:44 +0000

    Adds failing test case for batched base dist

--- tests/test_bijector.py
+++ tests/test_bijector.py
@@ -1,21 +1,29 @@
 # Copyright (c) FlowTorch Development Team. All Rights Reserved
 # SPDX-License-Identifier: MIT
+import pytest
+import torch
+import torch.distributions as dist
 
 import flowtorch
 import flowtorch.bijectors
 import flowtorch.params
 
-
-def test_bijector_constructor():
+@pytest.fixture
+def affine_ar_bijector():
     param_fn = flowtorch.params.DenseAutoregressive()
     b = flowtorch.bijectors.AffineAutoregressive(param_fn=param_fn)
-    assert b is not None
+    return b
 
 
-# class TestClass:
-#     def test_shapes(self):
-#         """
-#         Tests output shapes of bijector
-#         """
+def test_bijector_constructor(affine_ar_bijector):
+    assert affine_ar_bijector is not None
 
-#         assert "h" in x
+def test_simple(affine_ar_bijector):
+    base_dist = dist.Normal(0.0, 1.0)
+    new_dist, _ = affine_ar_bijector(base_dist)
+    assert new_dist.rsample((100,)).shape == (100, 1)
+
+def test_batched_base_distribution(affine_ar_bijector):
+    base_dist = dist.Normal(torch.zeros(1), torch.ones(1))
+    new_dist, _ = affine_ar_bijector(base_dist)
+    assert new_dist.rsample((100,)).shape == (100, 1)
