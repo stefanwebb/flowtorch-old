420e3bb6d3382f99a242e625a4c4153bbeb86e58
Feynman Liang <feynman.liang@gmail.com>
2021-01-13 12:47:24 -0800

    Linter

--- tests/test_bijector.py
+++ tests/test_bijector.py
@@ -14,12 +14,18 @@ def test_bijector_constructor():
     b = flowtorch.bijectors.AffineAutoregressive(param_fn=param_fn)
     assert b is not None
 
-@pytest.mark.parametrize("base_dist", [
-    dist.Normal(0.0, 1.0),
-    dist.Normal(torch.tensor(0.0), torch.tensor(1.0)),
-    dist.Normal(torch.tensor([0., 0.]), torch.tensor([1., 1.])),
-    dist.Independent(dist.Normal(torch.tensor([0., 0.]), torch.tensor([1., 1.])), 1),
-])
+
+@pytest.mark.parametrize(
+    "base_dist",
+    [
+        dist.Normal(0.0, 1.0),
+        dist.Normal(torch.tensor(0.0), torch.tensor(1.0)),
+        dist.Normal(torch.tensor([0.0, 0.0]), torch.tensor([1.0, 1.0])),
+        dist.Independent(
+            dist.Normal(torch.tensor([0.0, 0.0]), torch.tensor([1.0, 1.0])), 1
+        ),
+    ],
+)
 def test_batch_event_shapes(base_dist):
     bij = flowtorch.bijectors.AffineAutoregressive(
         flowtorch.params.DenseAutoregressive()
@@ -27,6 +33,7 @@ def test_batch_event_shapes(base_dist):
     tdist, _ = bij(base_dist)
     assert tdist.batch_shape == base_dist.batch_shape
     assert tdist.event_shape == base_dist.event_shape
+
 
 # class TestClass:
 #     def test_shapes(self):
