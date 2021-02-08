1797d0308369c959c5c16dadc3f777762125c3b8
Stefan Webb <info@stefanwebb.me>
2021-02-08 12:07:17 -0800

    Additional templates for website pages

---
+++ website/docs/users/shapes.md
@@ -0,0 +1,12 @@
+---
+id: shapes
+title: Shapes
+sidebar_label: Shapes
+---
+
+One of the advantages of using FlowTorch is that we have carefully thought out how the shape ...
+
+## Shape Conventions
+FlowTorch uses the same conventions to represent a random distributions shape and
+
+## Two Principles of Shapes
--- website/sidebars.js
+++ website/sidebars.js
@@ -1,12 +1,14 @@
 module.exports = {
   usersSidebar: {
     "Getting Started": ['users/intro', 'users/installation', 'users/start'],
-    "Basic Concepts": ['users/normalizing_flows', 'users/bijectors', 'users/parameters', 'users/transformed_distributions'],
+    "Normalizing Flows": ['users/univariate', 'users/multivariate', 'users/conditional'],
+    "Basic Concepts": ['users/shapes', 'users/constraints', 'users/bijectors', 'users/parameters', 'users/transformed_distributions', 'users/composing'],
     "Advanced Topics": ['users/caching', 'users/initialization', 'users/structure', 'users/torchscript'],
   },
   devsSidebar: {
     "General": ['dev/why', 'dev/contributing', 'dev/code_of_conduct', 'dev/about'],
     "Extending the Library": ['dev/overview', 'dev/docs', 'dev/tests', 'dev/bijector', 'dev/param'],
+    "Literature Survey": ['dev/prior', 'dev/libraries', 'dev/methodology', 'dev/applications'],
   },
   apiSidebar: {
     "Python API": ['api/flowtorch', 'api/bijectors', 'api/distributions', 'api/params', 'api/utils'],
