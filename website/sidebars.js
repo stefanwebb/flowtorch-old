c81c1c1829526414387070d3b802d95f5c8ac240
Stefan Webb <info@stefanwebb.me>
2021-03-16 17:04:03 -0700

    Placeholder for 'Serialization'

--- website/sidebars.js
+++ website/sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   usersSidebar: {
     "Getting Started": ['users/introduction', 'users/installation', 'users/start'],
     "Normalizing Flows": ['users/univariate', 'users/multivariate', 'users/conditional', 'users/methods'],
-    "Basic Concepts": ['users/shapes', 'users/constraints', 'users/bijectors', 'users/parameters', 'users/transformed_distributions', 'users/conditioning', 'users/composing', 'users/gpu_support'],
+    "Basic Concepts": ['users/shapes', 'users/constraints', 'users/bijectors', 'users/parameters', 'users/transformed_distributions', 'users/conditioning', 'users/composing', 'users/gpu_support', 'users/serialization'],
     "Advanced Topics": ['users/caching', 'users/initialization', 'users/structure', 'users/torchscript'],
   },
   devsSidebar: {
